030965db1b1431f1a226e60cb0533d04a75f2732
Louis Mario <louism4rio@gmail.com>
2023-10-30 19:14:19 +0700

    Slideshow

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,7 +13,9 @@
 
     <nav class="navbar">
         <div class="navbar-left">
-            <div class="navbar-logo">Nexus.co</div>
+            <div class="navbar-logo">
+                <img src="{{asset('assets/images/others/nexus.co.png')}}" alt="">
+            </div>
             <ul class="navbar-menu">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Games</a></li>
@@ -35,6 +37,10 @@
         <div class="slideshow-slide fade">
             <img src="{{asset('assets/images/games/resident_evil_4_remake.jpg')}}" alt="">
         </div>
+    </div>
+
+    <div class="footer-container">
+
     </div>
 
     <script src="{{asset('js/app.js')}}"></script>
